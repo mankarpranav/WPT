7afdf7b16c2f59a157775829bad7f2d7fe92ea8a
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-05-28 01:10:59 +0530

    Delete script.js

--- WPT-ALL/Daily-Practice-Codes/Day-6-Practice/Project-9(I)/script.js
+++
@@ -1,6 +0,0 @@
-var Principal = document.getElementById("id1").value;
-var interest = document.getElementById("id2").value;
-var period = document.getElementById("id3").value;
-
-var Compound = Principal(1 + (interest / period));
-document.write(Compound);

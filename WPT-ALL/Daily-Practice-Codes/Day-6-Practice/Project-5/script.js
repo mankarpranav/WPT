9d88d59a25f2d3877cfc11b76f986390a45454ad
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-05-28 01:32:52 +0530

    Delete script.js

--- WPT-ALL/Daily-Practice-Codes/Day-6-Practice/Project-5/script.js
+++
@@ -1,7 +0,0 @@
-function table() {
-    document.getElementById("result").innerHTML = " ";
-    var num = document.getElementById("id1").value;
-    for (var i = 1; i <= 10; i++) {
-        document.getElementById("result").innerHTML += " " + i + "*" + num + "=" + (i * num) + "<br>";
-    }
-}

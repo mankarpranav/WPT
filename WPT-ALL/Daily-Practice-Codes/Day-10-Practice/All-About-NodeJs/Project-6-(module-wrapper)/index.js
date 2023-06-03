7ec1d4613c8d1e221d337138b92869dcf3a84b33
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-06-03 23:43:48 +0530

    Add files via upload

---
+++ WPT-ALL/Daily-Practice-Codes/Day-10-Practice/All-About-NodeJs/Project-6-(module-wrapper)/index.js
@@ -0,0 +1,7 @@
+//run and debug and see how
+// values are assigned to variables and how callstack works on left side.
+
+require("./superman");
+require("./batman");
+
+console.log("inside index.js of Project-6");

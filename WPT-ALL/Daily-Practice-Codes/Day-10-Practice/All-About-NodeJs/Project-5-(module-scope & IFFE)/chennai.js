98bad88e55604111b6602679b300e44e51683132
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-06-03 23:43:24 +0530

    Add files via upload

---
+++ WPT-ALL/Daily-Practice-Codes/Day-10-Practice/All-About-NodeJs/Project-5-(module-scope & IFFE)/chennai.js
@@ -0,0 +1,12 @@
+const best = "Chennai Super Kings";
+console.log("Best Team of IPL : " + best);
+
+/*
+    Before execution nodejs wraps it as -->
+
+    (function () {
+        const best = "Chennai Super Kings";
+        console.log("Best Team of IPL : "+ best);
+    })();
+
+*/

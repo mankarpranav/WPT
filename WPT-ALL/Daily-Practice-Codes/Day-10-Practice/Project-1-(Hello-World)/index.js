af78af28d37fe3d6551b7b37d7775c2be8712e2d
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-06-05 00:47:01 +0530

    Delete index.js

--- WPT-ALL/Daily-Practice-Codes/Day-10-Practice/Project-1-(Hello-World)/index.js
+++
@@ -1,8 +0,0 @@
-// Writing hello world in file and executing js file in terminal -->
-
-console.log("Hello World from index.js");
-
-//can run code using command in terminal as-
-
-//  1. > node index.js    OR
-//  2. > node index
